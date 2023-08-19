a9b85e741c891c30d6e437de7b92e6357559def3
Patrick Lee <patricklee1138@gmail.com>
2023-08-18 17:52:16 -0700

    add basic animation

--- index.js
+++ index.js
@@ -9,14 +9,26 @@ canvas.height = 64 * 9; // 576
 c.fillStyle = 'white';
 c.fillRect(0, 0, canvas.width, canvas.height);
 
+let y = 100;
+const height = 100;
+let bottom = y + height;
 function animate() {
     // Dynamic function calls itself on 1st line to render indefinitely
     window.requestAnimationFrame(animate);
     console.log('animation running');
 
+    // Remember to clear the render at the start of each frame
+    c.fillStyle = 'white';
+    c.fillRect(0, 0, canvas.width, canvas.height);
+
     // 1st object
     c.fillStyle = 'red';
-    c.fillRect(100, 100, 100, 100);
+    c.fillRect(100, y, 100, height);
+
+    if (bottom < canvas.height) {
+        y++;
+        bottom = y + 100;
+    }
 }
 
 // Call dynamic function once, then it calls itself
