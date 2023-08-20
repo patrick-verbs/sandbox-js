866dd97a4465738a59317c19373a1616c1d07467
Patrick Lee <patricklee1138@gmail.com>
2023-08-19 19:53:12 -0700

    extract Card class to Card.js

---
+++ js/classes/Card.js
@@ -0,0 +1,30 @@
+class Card {
+    constructor() {
+        this.position = {
+            x: 100,
+            y: 100,
+        }
+
+        this.width = 100;
+        this.height = 100;
+
+        this.sides = {
+            bottom: this.position.y + this.height,
+            top: this.position.y,
+            left: this.position.x,
+            right: this.position.x + this.width,
+        }
+    }
+
+    draw() {
+        c.fillStyle = 'red';
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+
+    update() {
+        if (this.sides.bottom < canvas.height) {
+            this.position.y++;
+            this.sides.bottom = this.position.y + 100;
+        }
+    }
+}
