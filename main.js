03c5a45dfbf90a27384f31fd21d940f7af5d2104
Patrick Lee <patricklee1138@gmail.com>
2023-08-19 19:53:42 -0700

    rename 'index.js' -> 'main.js'

--- main.js
+++ main.js
@@ -4,37 +4,6 @@ const c = canvas.getContext('2d');
 canvas.width = 64 * 16; // 1024
 canvas.height = 64 * 9; // 576
 
-class Card {
-    constructor() {
-        this.position = {
-            x: 100,
-            y: 100,
-        }
-
-        this.width = 100;
-        this.height = 100;
-
-        this.sides = {
-            bottom: this.position.y + this.height,
-            top: this.position.y,
-            left: this.position.x,
-            right: this.position.x + this.width,
-        }
-    }
-
-    draw() {
-        c.fillStyle = 'red';
-        c.fillRect(this.position.x, this.position.y, this.width, this.height);
-    }
-
-    update() {
-        if (this.sides.bottom < canvas.height) {
-            this.position.y++;
-            this.sides.bottom = this.position.y + 100;
-        }
-    }
-}
-
 const card = new Card();
 
 // Static background renders once
